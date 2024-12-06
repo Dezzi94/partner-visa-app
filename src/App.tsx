46923b1fa0f0aed6901fddeb743d284621911ce6
Dezzi94 <techidesmond@gmail.com>
2024-12-06 01:43:51 +0000

    deployment
    fixing deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { ThemeProvider } from './contexts/ThemeContext';
